9d1b65ffedd5f631964cf48999c1f7f492060f59
Vladislav <GimliVlad@list.ru>
2024-08-07 17:07:24 +0300

    Add matrix extenshin method

--- Assets/Scripts/Utils/MatrixExtension.cs
+++ Assets/Scripts/Utils/MatrixExtension.cs
@@ -4,6 +4,8 @@ namespace Utils
 {
     public static class MatrixExtension
     {
+        private static readonly int _matrixElementsCount = 16;
+
         public static Vector3 GetScale(this Matrix4x4 matrix)
         {
             Vector3 scale;
@@ -13,6 +15,19 @@ namespace Utils
             scale.z = matrix.GetColumn(2).magnitude;
 
             return scale;
+        }
+
+        public static bool IsEqual(this Matrix4x4 matrix, Matrix4x4 comparableMatrix, float floatError = 0.001f)
+        {
+            for (int i = 0; i < _matrixElementsCount; i++)
+            {
+                if (Mathf.Abs(matrix[i] - comparableMatrix[i]) > floatError)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
