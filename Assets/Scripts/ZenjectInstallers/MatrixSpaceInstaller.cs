3b2218943541f43795939ddf0d44cc5141abb3c3
Vladislav <GimliVlad@list.ru>
2024-08-08 15:17:57 +0300

    Add calculation controller

--- Assets/Scripts/ZenjectInstallers/MatrixSpaceInstaller.cs
+++ Assets/Scripts/ZenjectInstallers/MatrixSpaceInstaller.cs
@@ -3,12 +3,15 @@ using Zenject;
 
 public class MatrixSpaceInstaller : MonoInstaller
 {
+    [SerializeField] private MatricesRenderer _matricesRenderer;
     [SerializeField] private OffsetsVisualizer _offsetsVisualizer;
 
     public override void InstallBindings()
     {
+        Container.Bind<IMatrixOffsetsFinder>().To<MatrixOffsetsFinder>().AsSingle();
         Container.Bind<IMatrixJsonConvert>().To<MatrixJsonConvert>().AsSingle();
         Container.Bind<ICubesCreator>().To<CubesCreator>().AsSingle();
+        Container.Bind<IMatricesRenderer>().FromInstance(_matricesRenderer);
         Container.Bind<IOffsetsVisualizer>().FromInstance(_offsetsVisualizer);
     }
 }
